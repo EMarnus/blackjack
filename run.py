4044486b1f1b487282a6bd76ae840175cbb8ebf1
Emile <bvcxzq1@gmail.com>
2023-03-09 20:29:13 +0000

    Add shuffle function, to randomise cards list

--- run.py
+++ run.py
@@ -8,3 +8,6 @@ ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
 for suit in suits:
   for rank in ranks:
     cards.append([suit, rank])
+
+def shuffle():
+  random.shuffle(cards)
