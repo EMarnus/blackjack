efe7cc946ec7e33ce2e4c1864084fcce6caefd19
Emile <bvcxzq1@gmail.com>
2023-03-09 20:26:31 +0000

    Add for loop to build cards

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ import random
 cards = []
 
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A","2","3","4","5","6","7","8","9","J","Q","K"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+
+for suit in suits:
+  for rank in ranks:
+    cards.append([suit, rank])
